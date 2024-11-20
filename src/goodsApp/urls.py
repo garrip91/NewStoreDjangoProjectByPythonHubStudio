983baf0be56e8fa9846b25d093042732691a3559
garrip91 <garrip91@gmail.com>
2024-11-20 15:56:59 +0300

    Simple search created

--- src/goodsApp/urls.py
+++ src/goodsApp/urls.py
@@ -5,6 +5,7 @@ from .views import catalog, product
 app_name = "goodsApp"
 
 urlpatterns = [
+    path("search/", catalog, name="search"),
     path("<slug:category_slug>/", catalog, name="index"),
     path("product/<slug:product_slug>/", product, name="product"),
 ]
