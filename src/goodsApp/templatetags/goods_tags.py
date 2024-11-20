8cc423e2d3df533049c097b8113428f0950b8c45
garrip91 <garrip91@gmail.com>
2024-11-20 10:30:44 +0300

    The product filter on the site page and urlencode

--- src/goodsApp/templatetags/goods_tags.py
+++ src/goodsApp/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 
 from goodsApp.models import Categories
 
@@ -10,3 +11,12 @@ register = template.Library()
 def tag_categories():
     
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    
+    return urlencode(query)
