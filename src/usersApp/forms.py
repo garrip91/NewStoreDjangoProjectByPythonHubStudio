3e72723f7fc6181fda0ac251fa144a3d82447446
garrip91 <garrip91@gmail.com>
2024-11-26 16:06:09 +0300

    10-28-30

--- src/usersApp/forms.py
+++ src/usersApp/forms.py
@@ -6,5 +6,19 @@ from .models import User
 
 class UserLoginForm(AuthenticationForm):
     
+    username = forms.CharField(
+        label = "Имя пользователя",
+        widget=forms.TextInput(attrs={"autofocus": True,
+                                      "class": "form-control",
+                                      "placeholder": "Введите Ваше имя пользователя",
+                                     }))
+    password = forms.CharField(
+        label = "Пароль",
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                          "class": "form-control",
+                                          "placeholder": "Введите Ваш пароль",
+                                         }))
+    
     class Meta:
         model = User
+        fields = ["username", "password"]
--- src/usersApp/views.py
+++ src/usersApp/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .forms import UserLoginForm
 
 
 def login(request):
     
-    form = UserLoginForm()
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("mainApp:index"))
+    else:
+        form = UserLoginForm()
+    
     context = {
         "title": "Home - Авторизация",
         "form": form,
