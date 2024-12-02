3e0326ec498f2585f810851a29192acec3e1f554
garrip91 <garrip91@gmail.com>
2024-12-02 11:54:01 +0300

    Add to cart view, refactor included cart templates

---
+++ src/cartsApp/templatetags/carts_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+from cartsApp.models import Cart
+
+
+register = template.Library()
+
+@register.simple_tag()
+def user_carts(request):
+    
+    if request.user.is_authenticated:
+        return Cart.objects.filter(user=request.user)
