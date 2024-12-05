6b35f08c7cf8bb161e1dc274d2ce6b9f466a21ae
garrip91 <garrip91@gmail.com>
2024-12-05 13:57:45 +0300

    Add-to-cart ajax request and json response

---
+++ src/cartsApp/utils.py
@@ -0,0 +1,7 @@
+from cartsApp.models import Cart
+
+
+def get_user_carts(request):
+    
+    if request.user.is_authenticated:
+        return Cart.objects.filter(user=request.user)
--- src/cartsApp/views.py
+++ src/cartsApp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
+from .models import Cart
+from .utils import get_user_carts
 
 from goodsApp.models import Products
-from cartsApp.models import Cart
 
 
-def cart_add(request, product_slug):
+def cart_add(request):
     
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
     
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -19,7 +24,17 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
     
-    return redirect(request.META["HTTP_REFERER"])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "cartsApp/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+    
+    response_data = {
+        "message": "Товар добавлен в корзину!",
+        "cart_items_html": cart_items_html,
+    }
+    
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
